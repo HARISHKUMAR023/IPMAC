3cfaa793ae674f3c690fab551090b4a88b8baf5a
HARISHKUMAR N <harishkumarsp023@gmail.com>
2023-12-12 11:51:47 +0530

    command-line tool for displaying the IP and MAC

---
+++ README.md
@@ -0,0 +1,25 @@
+# IP Finder Tool
+
+This is a simple command-line tool for displaying the IP and MAC addresses of the computer.
+
+## Installation
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory.
+3. Install the required Python packages using pip:
+
+```bash
+pip install -r requirements.txt
+
+Usage
+Run the tool using Python:
+     python setuprun.py
+You will be presented with a menu of options:
+
+Display IP address
+Display MAC address
+Exit
+Enter the number of the option you want to select.
+
+License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ setuprun.py
@@ -0,0 +1,38 @@
+from argparse import ArgumentParser, Namespace
+import socket
+import logo as lo
+from pyfiglet import Figlet
+import itertools
+def get_ip_address():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
+def main():
+    logo = lo.logo.split('\n')
+    tool_name = Figlet(font='slant').renderText('IP FINDER TOOL').split('\n')
+
+    tool_name = [''] * 5 + tool_name
+
+    for logo_line, name_line in itertools.zip_longest(logo, tool_name, fillvalue=''):
+        print(logo_line, name_line)
+
+    while True:
+        print("\n1. Display IP address")
+        print("2. Display MAC address")
+        print("00. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            print(f'IP address: {get_ip_address()}')
+        elif choice == '2':
+            print(f"MAC address option is not avilabe in this version.")
+            # print(f'MAC address: {get_mac_address()}')
+        elif choice == '00':
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
